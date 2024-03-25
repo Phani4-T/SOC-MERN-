0f2d4a9fe6f5e90d5b2db64ff4f05bc09311d799
Phani4-T <132120667+Phani4-T@users.noreply.github.com>
2024-03-25 11:31:39 +0530

    Add files via upload

--- NEW PROJECT-1/server.js
+++ NEW PROJECT-1/server.js
@@ -5,6 +5,9 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
 const cors=require('cors');
+const bcrypt=require('bcryptjs');
+const { rmSync } = require('fs');
+const SaltRound=10;
 // Initialize Express app
 const app = express();
 
@@ -22,12 +25,19 @@ const formDataSchema = new mongoose.Schema({}, { strict: false });
 
 // Create a Mongoose model based on the schema
 const FormData = mongoose.model('FormData', formDataSchema);
+//registerdata schema
+const registerformdata=new mongoose.Schema({
+    username:String,
+    email:String,
+    password:String
+});
+const RegisterData=mongoose.model('RegisterData',registerformdata);
 
 app.use(cors());
 // Middleware to parse JSON data
 app.use(express.json());
 app.set('view engine', 'pug');
-app.set('views', './');
+app.set('views', './views');
 
 app.use(bodyParser.json()); 
 
@@ -42,7 +52,63 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.render('form');
  });
- 
+ app.get('/register',(req,res)=>{
+    res.render('register');
+ })
+ app.get('/login',(req,res)=>{
+    res.render('login');
+ })
+
+ //Handling post requests 
+ app.post('/registerdata',(req,res)=>{
+    try{
+        if(req.body.newpassword === req.body.conformpassword)
+        {
+            bcrypt.hash(req.body.newpassword,SaltRound,function(err,hash){
+                const newuserData=new RegisterData({
+                    username:req.body.username,
+                    email:req.body.email,
+                    password:hash
+                });
+                newuserData.save();
+                res.render('login');
+            });
+            
+        }
+        else{
+            res.send(`alert('passwords not matched!..')`);
+        }
+    }catch(err){
+        res.status(400).json({message:err.message});
+    }
+ });
+ app.post('/logindata',async(req,res)=>{
+    try{
+        const username=req.body.username;
+        const password=req.body.password;
+        
+        await RegisterData.findOne({email:username},function(err,founduser){
+            if(err)
+            {
+                console.log(err);
+            }
+            else{
+                if(founduser){
+                    bcrypt.compare(password,founduser.password,function(err,result){
+                        if(result === true)
+                        {
+                            res.render('Homepage');
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+    catch(err){
+        res.status(400).json({message:err.message});
+    }
+ });
 // Create operation
 app.post('/formdata', async (req, res) => {
     try {
@@ -61,7 +127,6 @@ app.get('/formdata', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
-
 
 // Start the Express server
 const PORT = 3000;
